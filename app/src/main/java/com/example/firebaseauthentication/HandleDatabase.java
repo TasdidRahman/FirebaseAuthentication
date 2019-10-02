d16a6fd38e2bb5dc6be51b8b58cbff393813118a
Tasdid <rahman.tasdid@gmail.com>
2019-10-02 13:02:05 +0600

    storing data to RealtimeDatabase

---
+++ app/src/main/java/com/example/firebaseauthentication/HandleDatabase.java
@@ -0,0 +1,29 @@
+package com.example.firebaseauthentication;
+
+public class HandleDatabase {
+
+    private String name;
+    private String age;
+
+    public HandleDatabase(String name, String age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
--- app/src/main/java/com/example/firebaseauthentication/SecondActivity.java
+++ app/src/main/java/com/example/firebaseauthentication/SecondActivity.java
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SecondActivity extends AppCompatActivity {
 
-
+    private EditText edtName, edtAge;
     private Button btnLogout;
+    private Button btnSaveData;
 
     private FirebaseAuth mAuth;
+    DatabaseReference databaseReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +28,14 @@ public class SecondActivity extends AppCompatActivity {
         setContentView(R.layout.activity_second);
 
         mAuth = FirebaseAuth.getInstance();
+        databaseReference= FirebaseDatabase.getInstance().getReference("users");
 
+        
+        edtName=findViewById(R.id.edtName);
+        edtAge= findViewById(R.id.edtAge);
+        
         btnLogout = findViewById(R.id.btnLogout);
+        btnSaveData= findViewById(R.id.btnSaveData);
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,8 +43,29 @@ public class SecondActivity extends AppCompatActivity {
                 signOut();
             }
         });
+        btnSaveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
 
 
+
+
+    }
+
+    private void saveData() {
+
+        String name= edtName.getText().toString().trim();
+        String age= edtAge.getText().toString().trim();
+
+        String key= databaseReference.push().getKey();
+
+        HandleDatabase handleDatabase= new HandleDatabase(name, age);
+
+        databaseReference.child(key).setValue(handleDatabase);
+        Toast.makeText(this,"User name and age added", Toast.LENGTH_LONG).show();
 
 
     }
